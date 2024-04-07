07d5f91e09539f85037e9a89a6ac6ef481d2bfbd
Sidwyn Koh <sidwyn@gmail.com>
2024-04-07 09:20:57 -0700

    Update license and readme

--- core/includes/assets/js/backend-scripts.ts
+++ core/includes/assets/js/backend-scripts.ts
@@ -1,16 +1,6 @@
 import * as buffer from "buffer";
 import * as ReactDOM from "react-dom/client";
 
-import {
-  Connection,
-  Keypair,
-  PublicKey,
-  SystemProgram,
-  Transaction,
-  clusterApiUrl,
-  sendAndConfirmTransaction,
-} from "@solana/web3.js";
-
 import { SolanaPayContainer } from "./solana-pay-container";
 
 window.Buffer = buffer.Buffer;
@@ -19,70 +9,11 @@ window.Buffer = buffer.Buffer;
 Backend related javascript
 ------------------------*/
 
-// Your shop wallet address
-console.log("Hello World BACKENDER");
-
-export type MakeTransactionInputData = {
-  account: string;
-};
-
-export type MakeTransactionOutputData = {
-  transaction: string;
-  message: string;
-};
-
-const onClick = async () => {
-  const amountLamports = 1000;
-  const shopPublicKey = Keypair.generate().publicKey;
-  const buyerPublicKey = new PublicKey(
-    "BTv9YNRqryCoCq2S9pn6DaqygeyU5UK5x8DGSEWHSiHS"
-  ); // Need to update to buyer's public key
-  let payer = Keypair.generate();
-  let connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
-  debugger;
-
-  try {
-    // Create Simple Transaction
-    let transaction = new Transaction();
-
-    // Add an instruction to execute
-    transaction.add(
-      SystemProgram.transfer({
-        fromPubkey: buyerPublicKey,
-        toPubkey: shopPublicKey,
-        lamports: amountLamports,
-      })
-    );
-
-    // Send and confirm transaction
-    // Note: feePayer is by default the first signer, or payer, if the parameter is not set
-    await sendAndConfirmTransaction(connection, transaction, [payer]);
-
-    // Return the serialized transaction
-    console.log({
-      message: "Thanks for your order! 🍪",
-    });
-  } catch (err) {
-    console.error(err, { error: "error creating transaction" });
-    return;
-  }
-};
-
 document.addEventListener("DOMContentLoaded", function () {
   console.log("DOM ready");
-  document.querySelector("body").addEventListener("click", (ev) => {
-    if ((ev.target as HTMLElement).id == "hello-world-button") {
-      console.log("Clicked on hello world button");
-      onClick();
-    }
-  });
-
   const solanaPayContainer = document.getElementById(
     "solana-pay-main-container"
   );
   const modalRoot = ReactDOM.createRoot(solanaPayContainer);
-
-  // 1. Use wallet-adapter react to add and pay (should be simple)
   modalRoot.render(SolanaPayContainer());
 });
---
+++ readme.md
@@ -0,0 +1,28 @@
+## Solana Pay Wordpress Plugin
+
+This is a proof of concept for running Solana Pay on Wordpress / WooCommerce.
+
+It is currently in development. NOT ready for production unless you're ready to finagle the code.
+
+## Adding The Plugin
+
+To add the plugin to your WooCommerce store:
+
+1. Browse to the plugins page. Should look like https://{YOUR_STORE}/wp-admin/plugins.php
+2. Install npm dependencies
+3. Build the zip: `npm run zip`
+4. Upload the zip file as a plugin and activate it.
+
+## Wordpress vs Shopify
+
+Why build this plugin for Wordpress instead of Shopify? Well a couple of reasons:
+
+1. Wordpress is open-source.
+2. There's already a Solana Pay shopify integration at https://solanapay.com.
+3. Shopify Pay required partners to be approved to build payment apps. I believe there's some [revenue share](https://shopify.dev/docs/apps/payments/requirements#revenue-share) with Shopify going on.
+
+If the point of Solana is to reduce payment processing fees, why pay Shopify's revenue share?
+
+## License
+
+Apache License 2.0
